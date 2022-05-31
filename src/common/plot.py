3595488f9fc13a42912aa99e4d9eb1b0afc9d4ab
Jonas Röger <jonas.roeger@tu-dortmund.de>
2022-06-01 01:17:37 +0200

    plot functions

--- src/common/plot.py
+++ src/common/plot.py
@@ -1,5 +1,7 @@
+import imp
 import matplotlib as plt
 import numpy as np
+import networkx as nx
 
 
 def sample2DClassifier(img, classifier : callable, cmap, y0, y1, x0, x1, resolution = 100):
@@ -13,3 +15,39 @@ def sample2DClassifier(img, classifier : callable, cmap, y0, y1, x0, x1, resolut
   color_mesh = np.flip(cmap(classified_mesh / 4), axis=0)
 
   img.set(data=color_mesh, extent=(y0, y1, x0, x1))
+
+
+def plotWeightLayer(ax, weights : np.ndarray, dx : float, dy : float, cmap):
+  n_i, n_o = weights.shape
+
+  adj = np.hstack([np.zeros((n_i, n_i)), weights])
+  adj = np.vstack([adj, np.zeros((n_o, n_i+n_o))])
+
+  in_pos = [(x, (0,x*dx)) for x in range(0,n_i)]
+  out_pos = [(x, (dy,(x-n_i)*dx)) for x in range(n_i, n_i+n_o)]
+  pos = dict(in_pos + out_pos)
+
+  g = nx.from_numpy_matrix(adj, create_using=nx.DiGraph)
+
+  n_colors = [plt.colors.to_rgba('#888888')]
+  n_colors += [plt.colors.to_rgba('#DDDDDD')] * (n_i-1)
+  n_colors += map(lambda x : cmap(x/4), range(0, n_o))
+
+  e_colors = ['b' if d['weight'] < 0 else 'r' for u,v,d in g.edges.data()]
+
+  n_labels = ['B']
+  n_labels += [f'i_{x}' for x in range(0,n_i-1)]
+  n_labels += [f'o_{x}' for x in range(0,n_o)]
+  n_labels = dict(zip(range(0,n_i+n_o), n_labels))
+
+  e_labels = dict([((u,v,), f"{d['weight']:.2f}") for u,v,d in g.edges(data=True)])
+
+  widths = nx.get_edge_attributes(g, 'weight')
+  widths = np.array(list(widths.values()))
+  widths = 3*widths / np.max(np.abs(widths))
+  widths = list(widths)
+
+  nx.draw_networkx_nodes(g, ax=ax, pos=pos, node_color=n_colors)
+  nx.draw_networkx_labels(g, ax=ax, pos=pos, labels=n_labels)
+  nx.draw_networkx_edges(g, ax=ax, pos=pos, edge_color=e_colors, width=widths)
+  nx.draw_networkx_edge_labels(g, ax=ax, pos=pos, edge_labels=e_labels)
