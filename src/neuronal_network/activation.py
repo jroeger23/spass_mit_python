096ee54a2c40b38c2f4ebf33d1b269f7dfa7900e
Jonas Röger <jonas.roeger@tu-dortmund.de>
2022-06-04 02:43:52 +0200

    add softmax activation

--- src/neuronal_network/activation.py
+++ src/neuronal_network/activation.py
@@ -34,3 +34,24 @@ class ReLU(ActivationLayer):
 
   def __str__(self) -> str:
     return "ReLU"
+
+class SoftMax(ActivationLayer):
+  def __init__(self) -> None:
+    self.softmax = None
+
+  def forward(self, x_input: np.ndarray) -> npt.NDArray:
+    e = np.exp(x_input - np.c_[x_input.max(axis=1)]) # subtracting max cancels out
+    s = np.c_[e.sum(axis=1)]
+    self.softmax = np.divide(e, s, where=(s != 0.0))
+    return self.softmax
+
+  def backward(self, gradient: np.ndarray) -> npt.NDArray:
+    if self.softmax is None:
+      raise RuntimeError("SoftMax.backward(): no prior forward() call")
+
+    g = -self.softmax.dot(self.softmax)
+    np.fill_diagonal(g, self.softmax * (1 - self.softmax))
+    return g.dot(gradient)
+
+  def __str__(self) -> str:
+      return "softmax"
