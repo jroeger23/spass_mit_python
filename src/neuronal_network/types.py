0560a4203e785e4a7718b04ad538b55a9bd8e500
Jonas Röger <jonas.roeger@tu-dortmund.de>
2022-06-17 21:24:20 +0200

    add Optimizer type

--- src/neuronal_network/types.py
+++ src/neuronal_network/types.py
@@ -3,10 +3,10 @@ import numpy.typing as npt
 
 class NNLayer():
   def forward(self, x_input : np.ndarray) -> npt.NDArray:
-    raise NotImplementedError("NNLayer.forward() not implemented")
+    raise NotImplementedError(f"NNLayer.forward() not implemented")
 
   def backward(self, gradient : np.ndarray) -> npt.NDArray:
-    raise NotImplementedError("NNLayer.backward() not implemented")
+    raise NotImplementedError(f"NNLayer.backward() not implemented")
 
   def fit(self, learning_rate, momentum=0.01):
     pass
@@ -24,10 +24,23 @@ class ActivationLayer(NNLayer):
 
 class Loss:
   def __call__(self, x_input: np.ndarray) -> npt.NDArray:
-    raise NotImplementedError("Loss({self}).__call__() not implemented")
+    raise NotImplementedError(f"Loss({self}).__call__() not implemented")
 
   def gradient(self) -> npt.NDArray:
-    raise NotImplementedError("Loss({self}).gradient() not implemented")
+    raise NotImplementedError(f"Loss({self}).gradient() not implemented")
+
+  def __str__(self) -> str:
+    return ""
+
+class Optimizer:
+  def forward(self, x_input: np.ndarray) -> None:
+    pass
+
+  def backward(self, gradient: np.ndarray) -> None:
+    pass
+
+  def adjust(self, tgt_weight: np.ndarray) -> None:
+    raise NotImplementedError(f"Optimizer({self}).adjust() not implemented")
 
   def __str__(self) -> str:
     return ""
